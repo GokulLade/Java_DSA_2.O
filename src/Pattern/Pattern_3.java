1c814a16baadbc6f4f656494ecaa304066e0d009
Gokul <gokullade89@gmail.com>
2023-07-17 20:57:37 +0530

    Added Pattern 3 And Pattern 4 Program

---
+++ src/Pattern/Pattern_3.java
@@ -0,0 +1,56 @@
+package Pattern;
+
+public class Pattern_3 {
+    public static void main(String args[])
+    {
+        int n=5;
+        System.out.println("H Printing Using nested loops");
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=n;j++) {
+                if (i ==(n-1)/2 && j<3|| j == 0 || j == n-1)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println("L Printing Using nested loops");
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=n;j++) {
+                if (i==5 || j==0)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println("O Printing Using nested loops");
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=n;j++) {
+                if (j==0 &&i>1 && i<5 || i==1 && j>1 && j<4 ||j==4 && i>1 && i<5 || i==5 && j>0 && j<3 )
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
