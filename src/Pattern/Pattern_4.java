f98a6b0a8afca8a052f4a496050df78daa8f4850
Gokul <gokullade89@gmail.com>
2023-07-18 19:04:33 +0530

    Upadate Pattern4 and Add Pattern5

--- src/Pattern/Pattern_4.java
+++ src/Pattern/Pattern_4.java
@@ -3,7 +3,9 @@ package Pattern;
 public class Pattern_4 {
     public static void main(String args[])
     {
-        int n=10;
+        int n=15;
+        System.out.println("Line 1");
+        System.out.println();
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
@@ -20,6 +22,8 @@ public class Pattern_4 {
             System.out.println();
         }
 
+        System.out.println("Line 2");
+        System.out.println();
         for(int i=0;i<n;i++)
         {
             for (int j = 0; j < n; j++)
@@ -36,6 +40,8 @@ public class Pattern_4 {
             System.out.println();
         }
 
+        System.out.println("Line 3");
+        System.out.println();
         for(int i=0;i<n;i++)
         {
             for (int j = 0; j < n; j++)
@@ -52,6 +58,8 @@ public class Pattern_4 {
             System.out.println();
         }
 
+        System.out.println("Line 4");
+        System.out.println();
         for(int i=0;i<n;i++)
         {
             for (int j = 0; j < n; j++)
@@ -68,11 +76,31 @@ public class Pattern_4 {
             System.out.println();
         }
 
+        System.out.println("All lines");
+        System.out.println();
         for(int i=0;i<n;i++)
         {
             for (int j = 0; j < n; j++)
             {
-                if (i+j==(n-1)/2 || i-j==(n-1)/2 || j-i==(n-1)/2 || i+j==(n-1)+(n-1)/2)
+                if (i+j==(n-1)/2 || i-j==(n-1)/2 || j-i==(n-1)/2 || i+j==(n-1)+(n-1)/2 )
+                {
+                    System.out.print("*");
+                } else
+                {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+
+        System.out.println("Print X");
+        System.out.println();
+        for(int i=0;i<n;i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i==j || i+j==(n-1))
                 {
                     System.out.print("*");
                 } else
