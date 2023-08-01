22421dbd594ecc36ac944b10f8e09c6ea73350d0
Gokul Lade <97846356+GokulLade@users.noreply.github.com>
2023-08-01 20:24:20 +0530

    Update Pattern_1.java

--- src/Pattern/Pattern_1.java
+++ src/Pattern/Pattern_1.java
@@ -4,9 +4,9 @@ public class Pattern_1 {
     public static void main(String args[])
     {
         int n=5;
-        for(int i=0;i<=n;i++)
+        for(int i=0;i<=n;i++) // i=rows
         {
-            for(int j=0;j<=n;j++)
+            for(int j=0;j<=n;j++) // j=column
             {
                 System.out.print("*");
             }
