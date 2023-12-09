fac5ea2cb630420a0e9160954ed1bd450826ebd8
Gokul <gokullade89@gmail.com>
2023-12-09 13:39:13 +0530

    Add Switch case Demo Program

---
+++ src/BasicProgram/SwitchDemo.java
@@ -0,0 +1,51 @@
+package BasicProgram;
+
+import java.util.Scanner;
+
+public class SwitchDemo {
+    public static void main(String[] args)
+    {
+        int day;
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter The Day Number In Week");
+        day=sc.nextInt();
+
+        // Thursday, Friday, Saturday, and Sunday.
+        switch(day)
+        {
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            case 4:
+                System.out.println("Thursday");
+                break;
+
+            case 5:
+                System.out.println("Friday");
+                break;
+
+            case 6:
+                System.out.println("Saturday");
+                break;
+
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+            default:
+                System.out.println("Please Enter the Number between 1 to 7");
+                break;
+        }
+    }
+}
