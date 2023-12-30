f46de86377ec6a143aaca06bd1603a634c88fc4d
Gokul <gokullade89@gmail.com>
2023-12-30 19:36:16 +0530

    Delete data in database by using JDBC.

---
+++ src/JDBCPrograms/CRUDOperation/DeleteDate.java
@@ -0,0 +1,35 @@
+package JDBCPrograms.CRUDOperation;
+
+import JDBCPrograms.DatabaseConnection.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DeleteDate
+{
+    public static void main(String[] args) throws SQLException
+    {
+        //Create local variable
+        Connection con=null;
+        Statement stm=null;
+
+//Step-1--->Establish the connection
+        con= DatabaseConnection.getConnection();
+
+
+//Step-2--->Crate Statement
+        stm=con.createStatement();
+
+//Step-4--->Execute the query
+        stm.executeUpdate("Delete from Student where id=9");
+
+//Step-5--->Process the Result
+        System.out.println("Delete Data...");
+
+//Step-6--->close the resources
+        con.close();
+        stm.close();
+
+    }
+}
