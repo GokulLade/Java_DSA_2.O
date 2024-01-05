7134edacafaeb895312a3da08292a1e767bb1d20
Gokul <gokullade89@gmail.com>
2024-01-05 18:52:33 +0530

    Update Programs.

--- src/Array/Operations/SumOfArray.java
+++ src/Array/Operations/SumOfArray.java
@@ -5,24 +5,30 @@ import java.util.Scanner;
 public class SumOfArray {
     public static void main(String[] args)
     {
+        //Crete Scanner class ref
         Scanner sc=new Scanner(System.in);
 
+        //Array Declaration
         int a[]=new int[5];
 
+        //Variable Declaration
         int i,sum=0;
 
+        //Input Array Element
         System.out.println("Enter the value for Array Element  : ");
         for (i=0;i<a.length;i++)
         {
             a[i]=sc.nextInt();
         }
 
+        //Print Array Element
         System.out.println("Your Array Element are : ");
         for(i=0;i<a.length;i++)
         {
             System.out.println(a[i]);
         }
 
+        //Sum of Array Element
         System.out.print("Sum of array Element is : ");
         for (i=0;i<a.length;i++)
         {
--- src/Array/Operations/UserArraySize.java
+++ src/Array/Operations/UserArraySize.java
@@ -5,18 +5,24 @@ import java.util.Scanner;
 public class UserArraySize {
     public static void main(String[] args) {
 
+        //Create Scanner class ref
         Scanner sc=new Scanner(System.in);
 
+        //Input Array size from user
         System.out.println("Enter Array size ");
         int size=sc.nextInt();
+
+        //Declare Array
         int arr[]=new int [size];
 
+        //Input Array Element
         System.out.println("Please Enter Array Element : ");
         for(int i=0;i<arr.length;i++)
         {
             arr[i]=sc.nextInt();
         }
 
+        //Print Array Element
         System.out.println("Array Element are : ");
         for(int b : arr)
         {
