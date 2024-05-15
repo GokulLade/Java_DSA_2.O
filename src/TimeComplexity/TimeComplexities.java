749d652b0ca758822649380927aae003e690a58e
Gokul <gokullade89@gmail.com>
2024-05-16 00:15:00 +0530

    DSA-Time Complexity Program.

---
+++ src/TimeComplexity/TimeComplexities.java
@@ -0,0 +1,38 @@
+package TimeComplexity;
+
+//Finding Time Complexity
+
+import java.util.Scanner;
+
+public class TimeComplexities {
+    public static void main(String[] args)
+    {
+        // Time Complexity = 0(1)
+            for(int i=1;i<10;i++)
+            {
+                System.out.println("TimeComplexity");
+            }
+
+        // Time Complexity = 0(n)
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of N");
+        n=sc.nextInt();
+        for(int i=1;i<n;i++)
+        {
+            System.out.println("TimeComplexity");
+        }
+
+        // Time Complexity = 0(n*n)
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                System.out.println("TimeComplexity");
+            }
+
+        }
+
+
+    }
+}
