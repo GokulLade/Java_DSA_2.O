e480f75c125d05375e437412d5b12c1dba5b1f8d
Gokul <gokullade89@gmail.com>
2024-05-16 01:37:39 +0530

    Using Simple farmula find Sum of nature number

---
+++ src/BasicProgram/SumOfNatureNumber.java
@@ -0,0 +1,12 @@
+package BasicProgram;
+
+public class SumOfNatureNumber {
+    public static void main(String[] args) {
+        int number=6;
+        number=number+1;
+
+        int sum=number*(number+1)/2;
+
+        System.out.println(sum);
+    }
+}
