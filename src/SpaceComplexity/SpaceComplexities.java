bce8ea356df367e4306206f4fd467eb9ec39f8cc
Gokul <gokullade89@gmail.com>
2024-05-23 23:31:37 +0530

    Space Complexity program.

--- src/SpaceComplexity/SpaceComplexities.java
+++ src/SpaceComplexity/SpaceComplexities.java
@@ -1,7 +1,24 @@
 package SpaceComplexity;
 
+import java.util.Scanner;
+
 public class SpaceComplexities {
-    public static void main(String[] args) {
-        int a,b;
+    public static void main(String[] args)
+    {
+        // Space Complexity = 0(1)
+        for(int i=1;i<10;i++)
+        {
+            System.out.println("TimeComplexity");
+        }
+        // Time Complexity = 0(1)
+        int n=5;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                System.out.println("TimeComplexity");
+            }
+
+        }
     }
 }
