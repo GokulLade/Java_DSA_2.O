122f4130c843dda316a339c668296f0124652531
Gokul <gokullade89@gmail.com>
2024-09-01 22:14:41 +0530

    Naming Convention in Java Program

---
+++ src/com/nareshit/namingconvention/NamingConventionInJava.java
@@ -0,0 +1,30 @@
+
+//A package should be written in lowercase, usually the reverse of the company name.
+package com.nareshit.namingconvention;
+
+// While writing a class in Java, use Pascal naming convention.
+class ThisIsExampleOfClass{
+
+    //While writing a method in Java, use camel case naming convention.
+    public void thisIsExampleOfMethod(){
+
+        //While writing a variable in Java, use camel case naming convention.
+        String studentName;
+        int rollNumber;
+
+        //While writing the final variable in Java, use snake_case naming convention.
+        final int MIN_Value;
+        final int MAX_Value;
+
+    }
+
+}
+
+// Main Method Class
+public class NamingConventionInJava{
+    public static void main(String[] args) {
+        ThisIsExampleOfClass example=new ThisIsExampleOfClass();
+        example.thisIsExampleOfMethod();
+
+    }
+}
